3a3d892e56b6ca585f98087446e99c6859ecda48
Polina Ruyeva <60512525+pruyeva@users.noreply.github.com>
2023-06-01 15:07:59 +0200

    authentication features (#3)
    * add login view and accounts controller
    
    * add logout function and fix bugs

--- src/server.js
+++ src/server.js
@@ -5,9 +5,17 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
+import Cookie from "@hapi/cookie";
+import { accountsController } from "./controllers/accounts-controller.js";
+import dotenv from "dotenv";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+}
 
 async function init() {
   const server = Hapi.server({
@@ -15,6 +23,20 @@ async function init() {
     host: "localhost",
   });
   await server.register(Vision);
+  await server.register(Cookie);
+
+  server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: process.env.COOKIE_NAME,
+      password: process.env.COOKIE_PASSWORD,
+      isSecure: false,
+    },
+    redirectTo: "/",
+    validate: accountsController.validate,
+  });
+
+  server.auth.default("session");
+
   server.views({
     engines: {
       hbs: Handlebars,

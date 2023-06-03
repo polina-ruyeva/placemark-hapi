56fe2b9d5ae688ce474c136c39d450a3df69340f
Polina Ruyeva <60512525+pruyeva@users.noreply.github.com>
2023-06-03 16:20:54 +0200

    Joi validation (#5)
    * add joi schemas
    
    * add additional validation

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -1,4 +1,5 @@
 import { db } from "../models/db.js";
+import { UserCredentialsSpec, UserSpec } from "../models/joi-schemas.js";
 
 export const accountsController = {
   index: {
@@ -15,6 +16,13 @@ export const accountsController = {
   },
   signup: {
     auth: false,
+    validate: {
+      payload: UserSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("signup-view", { title: "Sign up error", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
       const user = request.payload;
       await db.userStore.addUser(user);
@@ -30,6 +38,13 @@ export const accountsController = {
 
   login: {
     auth: false,
+    validate: {
+      payload: UserCredentialsSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("login-view", { title: "Login error", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
       const { email, password } = request.payload;
       const user = await db.userStore.getUserByEmail(email);
